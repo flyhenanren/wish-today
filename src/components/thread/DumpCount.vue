3ca2dc6d10301856a89d1deb0867917121af1608
daichanghong <daichanghong@jiuqi.com.cn>
2025-03-07 10:06:24 +0800

    取消注释

--- src/components/thread/DumpCount.vue
+++ src/components/thread/DumpCount.vue
@@ -16,8 +16,8 @@ const startTime = ref<string>("1970-01-01 00:00:00")
 const endTime = ref<string>("9999-12-31 23:59:59")
 watch(() => props.selected,
   (value) => {
-    // initCountInfo(value)
-    // initThreadStatus(value)
+    initCountInfo(value)
+    initThreadStatus(value)
   },
   { deep: true})
 
